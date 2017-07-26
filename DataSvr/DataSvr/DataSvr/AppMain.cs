96353f4aa040c05b659ea7e709880be5a5128471
ohyeahwuhawuha <ohyeahwuhawuha3@gmail.com>
2017-07-26 16:36:06 +0800

    [Script]加了个MYSQL的服务器 虽然两边没有连起来

---
+++ DataSvr/DataSvr/DataSvr/AppMain.cs
@@ -0,0 +1,32 @@
+﻿using DataSvr.MySql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSvr
+{
+    class AppMain
+    {
+        private static void Test()
+        {
+            //List<List<string>> tttt = new List<List<string>>();
+            MySqlConnect.I.NotSelectAction("update level set cid = 'what' where id = '0fada017-c9bc-4dbc-8954-8ae5f0cc0b12'");
+            List<List<string>> tttt = MySqlConnect.I.SelectAction("select id, cid from level");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            MySqlConnect.I.Initialize();
+            NetMgr.I.OpenServer();
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+            NetMgr.I.CloseServer();
+        }
+    }
+}
---
+++ DataSvr/DataSvr/DataSvr/Script/MySql/MySqlConnect.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+//base from https://www.codeproject.com/articles/43438/connect-c-to-mysql
+
+namespace DataSvr.MySql
+{
+    class MySqlConnect : Singleton<MySqlConnect>
+    {
+        private string server = "localhost";
+        private string database = "fish";
+        private string user = "root";
+        private string password = "";
+        private string port = "3306";
+        private MySqlConnection connection;
+        private List<List<string>> selectResult = new List<List<string>>();
+
+        //Initialize values
+        public void Initialize()
+        {
+            string connectionString;
+            connectionString = "server=" + server + ";" + "user=" + user + ";" + "database=" +
+            database + ";" + "port" + port + ";" + "password=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert or Update or Delete statement
+        public void NotSelectAction(string sql)
+        {
+            //string query = "INSERT INTO tableinfo (name, age) VALUES('John Smith', '33')";
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }            
+        }
+
+        //Delete statement
+        //string query = "DELETE FROM tableinfo WHERE name='John Smith'";
+
+        //Select statement
+        public List<List<string>> SelectAction(string sql)
+        {
+            //string query = "SELECT * FROM tableinfo";
+
+            //Create a list to store the result
+            selectResult.Clear();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    List<string> oneRowResult = new List<string>();
+                    for (int index = 0; index < dataReader.FieldCount;++index)
+                    {
+                        oneRowResult.Add(dataReader[index].ToString());
+                    }
+                    selectResult.Add(oneRowResult);
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+            }
+            //return list to be displayed
+            return selectResult;
+        }
+
+//        //Count statement
+//        public int Count()
+//        {
+//            string query = "SELECT Count(*) FROM tableinfo";
+//            int Count = -1;
+//
+//            //Open Connection
+//            if (this.OpenConnection() == true)
+//            {
+//                //Create Mysql Command
+//                MySqlCommand cmd = new MySqlCommand(query, connection);
+//
+//                //ExecuteScalar will return one value
+//                Count = int.Parse(cmd.ExecuteScalar() + "");
+//
+//                //close Connection
+//                this.CloseConnection();
+//
+//                return Count;
+//            }
+//            else
+//            {
+//                return Count;
+//            }
+//        }
+    }
+}
